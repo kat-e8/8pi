252249438cd7fd0b86b6ada172fa6bf26863178d
kat-e8 <katlego.gagoopane@element8.co.za>
2026-01-31 11:24:50 +0200

    + annotationsUpage ctrl

--- app_api/controllers/annotations.js
+++ app_api/controllers/annotations.js
@@ -34,9 +34,50 @@ const annotationsReadOne = (req, res) => {
 };
 
 const annotationsUpdateOne = (req, res) => {
-    res
-        .status(200)
-        .json({"status": "success"});
+    if(!req.params.tagid || !req.params.annotationid){
+        return res
+                .status(400)
+                .json({"message": "tag id and annotation id required"});
+    } else {
+        Tag
+        .findById(req.params.tagid)
+        .select('annotations')
+        .then((tag) => {
+            if (tag.annotations && tag.annotations.length > 0) {
+                annotation = tag.annotations.id(req.params.annotationid)
+                if(!annotation){
+                    return res
+                        .status(404)
+                        .json({"message": "annotation not found"});
+                }
+                else{
+                    if(req.body.author){
+                        annotation.author = req.body.author;
+                    }
+                    if(req.body.comment){
+                        annotation.comment = req.body.comment;
+                    }
+                    tag
+                        .save()
+                        .then((tag) => {
+                            return res
+                                    .status(200)
+                                    .json(annotation);
+
+                        }).catch((err) => {
+                            return res
+                                    .status(404)
+                                    .json({"message": "could not save changes."});
+                        });   
+                }
+            }
+        }).catch((err) => {
+            return res
+                .status(404)
+                .json({"message": "tag not found"});
+        });
+
+    }
 };
 
 const annotationsDeleteOne = (req, res) => {
